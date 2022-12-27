ffff9c606077f39f41f8411dee7b7f2689b4381a
thiiiimarques <tiagomarques2283@hotmail.com>
2022-12-27 15:59:17 -0300

    primeira classe Posicão criada

--- src/aplicacao/Execucao.java
+++ src/aplicacao/Execucao.java
@@ -1,10 +1,14 @@
 package aplicacao;
 
+import tabuleiro.Posicao;
+
 public class Execucao {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Criando projeto e associando ao GitHub");
+		Posicao posicao = new Posicao(3, 5);
+		
+		System.out.println(posicao);
 
 	}
 
