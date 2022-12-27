1e0383f50c233936e48b781401ff23e1f0297849
thiiiimarques <tiagomarques2283@hotmail.com>
2022-12-27 16:16:47 -0300

    Começando a implementação das classes Tabuleiro e Peça

--- src/aplicacao/Execucao.java
+++ src/aplicacao/Execucao.java
@@ -1,14 +1,14 @@
 package aplicacao;
 
 import tabuleiro.Posicao;
+import tabuleiro.Tabuleiro;
 
 public class Execucao {
 
 	public static void main(String[] args) {
 		
-		Posicao posicao = new Posicao(3, 5);
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 		
-		System.out.println(posicao);
 
 	}
 
