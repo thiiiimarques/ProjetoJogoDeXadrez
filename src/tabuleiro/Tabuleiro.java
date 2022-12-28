a0cb06711e9658397cf12cea7bae19c6a0146a3c
thiiiimarques <tiagomarques2283@hotmail.com>
2022-12-28 11:05:56 -0300

    Criação de exceções e metodos condicionais para uma programação mais defensiva

--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -8,6 +8,10 @@ public class Tabuleiro {
 	private Peca[][] pecas;
 	
 	public Tabuleiro(int linhas, int colunas) {
+		if(linhas < 1 || colunas < 1) {
+			throw new ExcecaoTabuleiro("Erro na criação do tabuleiro! Para se jogar xadrez vc precisa de Tabuleiro 8x8");
+		}
+		
 		this.linhas = linhas;
 		this.colunas = colunas;
 		pecas = new Peca[linhas][colunas];
@@ -19,31 +23,45 @@ public class Tabuleiro {
 		return linhas;
 	}
 
-	public void setLinhas(int linhas) {
-		this.linhas = linhas;
-	}
-
 	public int getColunas() {
 		return colunas;
 	}
-
-	public void setColunas(int colunas) {
-		this.colunas = colunas;
-	}
 	
 	public Peca peca(int linha, int coluna) {
+		if (!existePosicao(linha, coluna)) {
+			throw new ExcecaoTabuleiro("Essa posição não existe dentro deste tabuleiro!");
+		}
 		return pecas[linha][coluna];
 	}
 	
 	public Peca peca(Posicao posicao) {
+		if(!existePosicao(posicao)) {
+			throw new ExcecaoTabuleiro("Essa posição não existe dentro deste tabuleiro!");
+		}
 		return pecas[posicao.getLinha()][posicao.getColuna()];
 	}
 	
 	public void colocarPecaTabuleiro(Peca peca, Posicao posicao) {
+		if(existePeca(posicao)) {
+			throw new ExcecaoTabuleiro("Oxe, já existe uma peça nesta posição!! A peça está na posição: " + posicao);
+		}
 		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
 		peca.posicao = posicao;
 	}
 	
+	private boolean existePosicao(int linha, int coluna) {
+		return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+	}
 	
+	public boolean existePosicao(Posicao posicao) {
+		return existePosicao(posicao.getLinha(), posicao.getColuna());
+	}
+	
+	public boolean existePeca(Posicao posicao) {
+		if(!existePosicao(posicao)) {
+			throw new ExcecaoTabuleiro("Essa posição não existe dentro deste tabuleiro!");
+		}
+		return peca(posicao) != null;
+	}
 	
 }
